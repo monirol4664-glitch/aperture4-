c32acd7d5e2f93ee7f4546294f294c9799dedf9f
monirol4664-glitch <monirol4664@gmail.com>
2026-04-18 19:05:22 +0600

    Add metadata for version, author, and license

--- src/mathematica_console/__init__.py
+++ src/mathematica_console/__init__.py
@@ -2,7 +2,10 @@
 Mathematica Console - Offline Symbolic Math Application
 """
 
+__version__ = "1.0.0"
+__author__ = "Your Name"
+__license__ = "MIT"
+
 from .main import main
 
-__version__ = '1.0.0'
 __all__ = ['main']
