33b98f3ef0dfa39576263ffbcdfdfceaba608639
monirol4664-glitch <monirol4664@gmail.com>
2026-04-20 14:25:24 +0600

    Delete CalculusEngine.cs

--- Services/CalculusEngine.cs
+++
@@ -1,9 +0,0 @@
-namespace MathToolsApp.Services;
-
-public class CalculusEngine
-{
-    public double Derivative(Func<double, double> f, double x, double h = 1e-6)
-    {
-        return (f(x + h) - f(x - h)) / (2 * h);
-    }
-}
